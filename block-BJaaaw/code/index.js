679e00f07b7a151729b2d665e035d6de0e2a560f
rcc0404 <rahulchaudhar3@gmail.com>
2022-04-24 10:55:55 +0530

    Making Decision In Code

--- block-BJaaaw/code/index.js
+++ block-BJaaaw/code/index.js
@@ -6,6 +6,12 @@
   - `if` the number is even print the message " number is even"
   - `if` the number is odd print the message "number is odd"
 */
+let even = 3;
+if (even = 2) {
+  alert("number is even");
+} else {
+  alert("number is odd")
+}
 
 // 2. Write a program to accept two numbers from user using`prompt` and alert the max value.
 
